c2b2c3de10f8d0f02a060132be7b763d750293eb
Verlene Dodds <kylan.dodds@gmail.com>
2022-02-15 18:22:26 -0700

    Update auto-deploy.sh

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -4,9 +4,9 @@
 # Install github-desktop
 sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
 sudo apt-get install gdebi-core -y
-sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
+sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb -y
 
-clear
+
 
 # Install VS-code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -17,18 +17,21 @@ sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code -y
 
-clear
+
 
 # Install docker
 sudo apt-get install docker -y
 sudo apt-get install docker.io -y
 
-clear
+
 
 # Install Curl
 sudo apt-get install curl -y
 
-clear
+
+
+# Make Nexus folder
+mkdir ~/Nexus
 
 # Grab Dockerfile
 curl https://raw.githubusercontent.com/VerleneDodds/Nexus/main/Dockerfile >> ~/Nexus/Dockerfile
@@ -36,5 +39,10 @@ curl https://raw.githubusercontent.com/VerleneDodds/Nexus/main/Dockerfile >> ~/N
 # Build Dockerfile
 sudo docker build ~/Nexus
 
+# Get image ID 
+ID=$(sudo docker images --format "{{.ID}}" --filter "dangling=true")
 
+#Deploy Nexus container
+
+sudo docker run --name Nexus -t -i --privileged -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9443:9443 -v /var/run -v /var/lib/docker/volumes $ID
 
