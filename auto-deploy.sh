26e5b3d4e34a6d046318db90595720fb0665eaa1
Verlene Dodds <kylan.dodds@gmail.com>
2022-02-16 15:18:49 -0700

    Update auto-deploy.sh

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -13,9 +13,9 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
-sudo apt update
-sudo apt install code -y
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y
 
 clear
 
