97096664b3c4e08c9aff168b445faba1cfbacc65
Verlene Dodds <kylan.dodds@gmail.com>
2022-04-16 09:15:46 -0600

    Update detect-distro.sh

--- detect-distro.sh
+++ detect-distro.sh
@@ -3,23 +3,23 @@
 Linux_version=$(grep -o 'fedora\|debian\|arch\|slackware\|unix\|freebsd' /etc/os-release | uniq)
 
 
-if [ "$Linux_version" = "fedora"]
+if ["$Linux_version" = "fedora"]
 then
-    # Install github-desktop      
+    #Install github-desktop      
     sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb     
     sudo yum install -y alien
     sudo alien -r --scripts GitHubDesktop-linux-2.9.3-linux3.deb
     sudo rpm -i github-desktop-2.9.3-1.x86_64.rpm
     clear
 
-    # Install VS-codey
+    #Install VS-codey
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo echo -e "[code] \nname=Visual Studio Code \nbaseurl=https://packages.microsoft.com/yumrepos/vscode \nenabled=1 \ngpgcheck=1 \ngpgkey=https://packages.microsoft.com/keys/microsoft.asc " > /etc/yum.repos.d/vscode.repo
     sudo chown root /etc/yum.repos.d/vscode.repo
     sudo yum install -y code
     clear
 
-    # Install Docker
+    #Install Docker
     sudo yum -y install dnf-plugins-core
     sudo yum config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
     sudo yum install -y docker-ce docker-ce-cli containerd.io
@@ -27,7 +27,7 @@ then
         
     clear
 
-    # Make Nexus folder
+    #Make Nexus folder
     mkdir ~/Nexus
 
     #Grab Dockerfile
@@ -35,26 +35,26 @@ then
 
     clear
 
-    # Build Dockerfile
+    #Build Dockerfile
     sudo docker build ~/Nexus
 
     clear
 
-    # Get Image ID
+    #Get Image ID
     ID=$(sudo docker images --format "{{.ID}}" --filter "dangling=true")
 
-    # Deploy Nexus Container
+    #Deploy Nexus Container
     sudo docker run --name Nexus -t -i --privileged --init -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9443:9443 -v /var/run -v /var/lib/docker/volumes $ID
 
-elif [ "$Linux_version" = "debian"]
-    # Install github-desktop      
+elif ["$Linux_version" = "debian"]
+    #Install github-desktop      
     sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb     
     sudo apt-get install gdebi-core -y    
     sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb -y    
 
     clear     
 
-    # Install VS-code     
+    #Install VS-code     
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg      
     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/    
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'      
@@ -65,65 +65,96 @@ elif [ "$Linux_version" = "debian"]
 
     clear     
 
-    # Install docker      
+    #Install docker      
     sudo apt-get install docker -y    
     sudo apt-get install docker.io -y     
 
     clear     
 
-    # Install Curl    
+    #Install Curl    
     sudo apt-get install curl -y      
 
     clear     
 
-    # Make Nexus folder   
+    #Make Nexus folder   
     mkdir ~/Nexus     
 
-    # Grab Dockerfile     
+    #Grab Dockerfile     
     curl https://raw.githubusercontent.com/VerleneDodds/Nexus/main/Dockerfile >> ~/Nexus/Dockerfile   
 
-    # Build Dockerfile    
+    #Build Dockerfile    
     sudo docker build ~/Nexus     
 
     clear     
 
-    # Get image ID    
+    #Get image ID    
     ID=$(sudo docker images --format "{{.ID}}" --filter "dangling=true")      
 
     #Deploy Nexus container   
 
     sudo docker run --name Nexus -t -i --privileged --init -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9443:9443 -v /var/run -v /var/lib/docker/volumes $ID     
 
-#elif [ "$Linux_version" = "arch"]
-    # Make non_root user
-    #echo 'non_root ALL=NOPASSWD: ALL' >> /etc/sudoers
-    #mkdir /home/non_root
-    #chown -R non_root:non_root /home/non_root
+elif ["$Linux_version" = "arch"]
 
-    #change permissions
-    #sudo chmod 777 
+    #Update repositories 
+    sudo pacman -Syy
 
-    # Install snap
-    #git clone https://aur.archlinux.org/snapd.git
-    #cd snapd
-    #sudo -u nobody makepkg -si
+    clear
 
-    # Enable snap
-    #sudo systemctl enable --now snapd.socket
-    #sudo ln -s /var/lib/snapd/snap /snap
+    #Install and enable docker
+    sudo pacman -S docker --noconfirm
+    sudo systemctl start docker.service
+    sudo systemctl enable docker.service
 
-    #cd ~
+    clear
 
+    #Add user to Docker group
+    sudo usermod -aG docker $USER
+
+    clear
+
+    #Install git
+    sudo pacman -S git --noconfirm
+
+    clear
+    
     #Install VS-code
-    #curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/visual-studio-code-bin.tar.gz
-    #tar -xvf visual-studio-code-bin.tar.gz
-    #cd visual-studio-code-bin
-    #sudo -u nobody makepkg -si
+    cd ~/Downloads
+    git clone https://AUR.archlinux.org/visual-studio-code-bin.git
+    cd visual-studio-code-bin/
+    sudo makepkg -s --noconfirm
+    sudo pacman -U visual-studio-code-bin-*.pkg.tar.xz --noconfirm
+    cd ../ && sudo rm -rfv visual-studio-code-bin/
+
+    clear
+
+    #Install Curl    
+    pacman -Sy curl --noconfirm   
+
+    clear     
+
+    #Make Nexus folder   
+    mkdir ~/Nexus     
+
+    #Grab Dockerfile     
+    curl https://raw.githubusercontent.com/VerleneDodds/Nexus/main/Dockerfile >> ~/Nexus/Dockerfile   
+
+    #Build Dockerfile    
+    sudo docker build ~/Nexus     
+
+    clear     
+
+    #Get image ID    
+    ID=$(sudo docker images --format "{{.ID}}" --filter "dangling=true")      
+
+    #Deploy Nexus container   
+
+    sudo docker run --name Nexus -t -i --privileged --init -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9443:9443 -v /var/run -v /var/lib/docker/volumes $ID     
 
 
-#elif [ "$Linux_version" = "slackware"]
+#elif ["$Linux_version" = "slackware"]
 #    STATEMENTS4
-#elif [ "$Linux_version" = "unix" || "$Linux_version" = "freebsd"]
+#elif ["$Linux_version" = "unix" || "$Linux_version" = "freebsd"]
 #    STATEMENTS5
 else
     echo 'Unable to detect distro version'
